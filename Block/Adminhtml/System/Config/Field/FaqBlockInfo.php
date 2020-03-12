4e21e989932aa5950fe687dd15898c1eade0e908
mageprince <mail.mageprince@gmail.com>
2020-03-12 15:06:19 +0530

    Add cms and static block info

---
+++ Block/Adminhtml/System/Config/Field/FaqBlockInfo.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * MagePrince
+ * Copyright (C) 2020 Mageprince <info@mageprince.com>
+ *
+ * @package Mageprince_Faq
+ * @copyright Copyright (c) 2020 Mageprince (http://www.mageprince.com/)
+ * @license http://opensource.org/licenses/gpl-3.0.html GNU General Public License,version 3 (GPL-3.0)
+ * @author MagePrince <info@mageprince.com>
+ */
+
+namespace Mageprince\Faq\Block\Adminhtml\System\Config\Field;
+
+class FaqBlockInfo extends \Magento\Config\Block\System\Config\Form\Field
+{
+    /**
+     * Template path
+     *
+     * @var string
+     */
+    protected $_template = 'Mageprince_Faq::system/config/Field/FaqBlockInfo.phtml';
+
+    /**
+     * Render fieldset html
+     *
+     * @param \Magento\Framework\Data\Form\Element\AbstractElement $element
+     * @return string
+     */
+    public function render(\Magento\Framework\Data\Form\Element\AbstractElement $element)
+    {
+        return $this->_decorateRowHtml($element, "<td colspan='2'>" . $this->toHtml() . '</td>');
+    }
+}
